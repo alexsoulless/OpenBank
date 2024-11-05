d3c5dc7e8b5aa9b7bb3deb0acbf8116d0f75bb6b
alexsoulless <onilove.sa@gmail.com>
2024-11-05 05:33:09 +0300

    getUser и OBclasses

--- app/dbRequests.py
+++ app/dbRequests.py
@@ -2,6 +2,7 @@ import mysql.connector
 from mysql.connector import pooling
 import mysql.connector.cursor
 from config import DB_PASSWORD, DB_USER, DB_HOST, DB_NAME, DB_PORT
+from OBclasses import User
 
 
 def connectionPool() -> pooling.MySQLConnectionPool | None:
@@ -18,12 +19,91 @@ def connectionPool() -> pooling.MySQLConnectionPool | None:
         return connection_pool
     except Exception:
         return None
-    
-def getConnection(pool : pooling.MySQLConnectionPool) -> pooling.PooledMySQLConnection:
+
+
+def getConnection(pool: pooling.MySQLConnectionPool) -> pooling.PooledMySQLConnection:
     return pool.get_connection()
 
-def getCursor(conn : pooling.PooledMySQLConnection) -> mysql.connector.cursor:
+
+def getCursor(conn: pooling.PooledMySQLConnection) -> mysql.connector.cursor:
+    """Возвращает курсор. Курсор буферизирванный, извлакать данные нужно как из генератора/итератора
+
+    Args:
+        conn (pooling.PooledMySQLConnection): соединение
+
+    Returns:
+        mysql.connector.cursor: курсор
+    """
     return conn.cursor(buffered=True)
+
+
+def getUser(
+    pool: pooling.MySQLConnectionPool,
+    id: int | None = None,
+    username: str | None = None,
+    FIO: str | None = None,
+) -> dict | None:
+    """Возвращает всю информацию о пользователе по id или username или ФИО. ВАЖНО! Использовать только с 1 из критериев отбора. 
+
+    Args:
+        pool (pooling.MySQLConnectionPool): пул соединений
+        id (int | None, optional): ID пользователя. Defaults to None.
+        username (str | None, optional): имя пользователя. Defaults to None.
+        FIO (str | None, optional): ФИО пользователя. Defaults to None.
+
+    Returns:
+        dict | None: Все данные о пользователе в формате словаря:
+        {
+            "id": id,
+            "username": username,
+            "FIO": FIO,
+            "balance": balance,
+            "isBanned": isBanned,
+            "isOrg": isOrg
+        }
+        Если пользователя не нашлось, словарь пустой.
+        
+        При ошибке выполнения функции возвращает None (напр. передано несколько критериев)
+    """    
+    if [id, username, FIO].count(None) != 2:
+        return None
+
+    conn = getConnection(pool)
+    cursor = getCursor(conn)
+
+    query = """
+SELECT * FROM users
+WHERE {} = {}
+"""
+    values = ()
+    if id is not None:
+        values = ("id", f"{id}")
+    elif username is not None:
+        values = ("username", f"'{username}'")
+    else:
+        values = ("FIO", f"'{FIO}'")
+
+    cursor.execute(query.format(*values))
+    try:
+        res = next(cursor)
+    except Exception:
+        return {}
+
+    cursor.close()
+    conn.close()
+
+    if res:
+        id, username, FIO, balance, isBanned, isOrg = res
+        return {
+            "id": id,
+            "username": username,
+            "FIO": FIO,
+            "balance": balance,
+            "isBanned": isBanned,
+            "isOrg": isOrg,
+        }
+    else:
+        return {}
 
 
 if __name__ == "__main__":
@@ -31,19 +111,5 @@ if __name__ == "__main__":
     if pool == None:
         print("error on db connect")
         exit(1)
-    
-    conn = getConnection(pool)
-    cursor = getCursor(conn)
-    
-    query = """
-SHOW COLUMNS FROM users
-"""
-    cursor.execute(query)
-    
-    for line in cursor:
-        print(line)
 
-    cursor.close()
-    conn.close()
-    
-    
+    print(getUser(pool, FIO="Абрамович Александр Владимирович"))
